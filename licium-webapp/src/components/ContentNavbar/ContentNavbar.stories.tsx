751b211fdc74fb3b17c3ac5d1f4a66f378e89eda
Andreas Houben <andreas.houben@googlemail.com>
2020-07-20 21:28:25 +0200

    tests

--- licium-webapp/src/components/ContentNavbar/ContentNavbar.stories.tsx
+++ licium-webapp/src/components/ContentNavbar/ContentNavbar.stories.tsx
@@ -1,43 +1,38 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { StoryFn } from '@storybook/addons';
-import { storiesOf } from '@storybook/react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { goToSection } from '../../store/navigation/navigationSlice';
-import { store } from '../../store/store';
-import { NavBar } from "./ContentNavbar";
-
-
-
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { StoryFn } from '@storybook/addons'
+import { storiesOf } from '@storybook/react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { goToSection } from '../../store/navigation/navigationSlice'
+import { store } from '../../store/store'
+import { NavBar } from './ContentNavbar'
+import { Navbar } from 'react-bootstrap'
 
 const withProvider = (story: StoryFn<any>) => (
-    <Provider store={store}>
-        {story()}
-    </Provider>
+  <Provider store={store}>{story()}</Provider>
 )
 
+export default {
+  title: 'Navbar',
+  component: Navbar,
+  decorators: [withProvider],
+}
 
-storiesOf('Navbar', module)
-    .addDecorator(withProvider)
-    .add('default', () => <NavBar></NavBar>)
-    .add('Identification selected', () => {
-        store.dispatch(goToSection('id'))
-        return <NavBar></NavBar>
-    })
-    .add('Registration selected', () => {
-        store.dispatch(goToSection('reg'))
-        return <NavBar></NavBar>
-    })
-    .add('Certification selected', () => {
-        store.dispatch(goToSection('cert'))
-        return <NavBar></NavBar>
-    })
-    .add('Verification selected', () => {
-        store.dispatch(goToSection('veri'))
-        return <NavBar></NavBar>
-    })
-
-
-
-
+export const Default = () => <Navbar></Navbar>
+export const IdentificationSelected = () => {
+  store.dispatch(goToSection('id'))
+  return <NavBar></NavBar>
+}
+export const RegistrationSelected = () => {
+  store.dispatch(goToSection('reg'))
+  return <NavBar></NavBar>
+}
+export const CertificationSelected = () => {
+  store.dispatch(goToSection('cert'))
+  return <NavBar></NavBar>
+}
+export const VerificationSelected = () => {
+  store.dispatch(goToSection('veri'))
+  return <NavBar></NavBar>
+}
