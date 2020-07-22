d67fc9674c2b8ea24c65efc8fcd1d0868c4c5ff3
Andreas Houben <andreas.houben@codecentric.de>
2020-07-22 10:22:53 +0200

    import logo as svg

--- licium-webapp/src/components/Logo/Logo.tsx
+++ licium-webapp/src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import logo from './Logo.svg'
+import { ReactComponent as LogoSVG } from './Logo.svg'
 import './Logo.css'
 
 type Size = 'sm' | 'm' | 'l'
 
 export function Logo(props: { size?: Size }) {
-  return <img src={logo} alt="Application Logo" className={props.size} />
+  return <LogoSVG height="auto" className={props.size} />
 }
