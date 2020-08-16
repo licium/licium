9f0ed1291b911ff88cd75de3d57d8a1b35013868
Andreas Houben <andreas.houben@codecentric.de>
2020-08-16 14:15:02 +0200

    Revert "Revert "Remove react-redux because of this tweet: https://twitter.com/Twittfort/status/1292742609857908737""
    This reverts commit 105e0224

--- licium-webapp/src/index.tsx
+++ licium-webapp/src/index.tsx
@@ -2,21 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
-import { store } from './store/store'
-import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulma/css/bulma.min.css'
-import { addItems } from './store/asset/assetSlice'
-import { manyDemoItems } from './components/AssetsTable/manyDemoItems'
-
-store.dispatch(addItems(manyDemoItems))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
