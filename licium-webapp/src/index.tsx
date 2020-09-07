73a08935717bf23f164fa9f275ed0ac6342e0ad8
Andreas Houben <andreas.houben@codecentric.de>
2020-09-07 22:09:39 +0200

    disable drizzle for now

--- licium-webapp/src/index.tsx
+++ licium-webapp/src/index.tsx
@@ -5,24 +5,12 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulma/css/bulma.min.css'
-import Greeter from './assets/contracts/Greeter.json'
-// @ts-ignore
-import { DrizzleContext } from '@drizzle/react-plugin'
-// @ts-ignore
-import { Drizzle } from '@drizzle/store'
 
-const drizzleOptions = {
-  contracts: [Greeter],
-  events: {},
-}
-
-const drizzle = new Drizzle(drizzleOptions as any)
+//const drizzle = new Drizzle(drizzleOptions as any)
 
 ReactDOM.render(
   <React.StrictMode>
-    <DrizzleContext.Provider drizzle={drizzle}>
-      <App />
-    </DrizzleContext.Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
