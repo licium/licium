ef1d8553462a6a3e41b39949853055805b1513e2
Andreas Houben <andreas.houben@codecentric.de>
2020-09-07 21:28:36 +0200

    Enable ISCC Generation by file upload

--- licium-webapp/src/components/ISCCRegistration/ISCCRegistration.tsx
+++ licium-webapp/src/components/ISCCRegistration/ISCCRegistration.tsx
@@ -1,29 +1,71 @@
 import React, { FormEvent, useState } from 'react'
 import ISCC, { ISCCCode } from '../ISCC/ISCC'
 import { API_PATH } from '../../App'
+import Dropzone from 'react-dropzone'
+import './ISCCRegistration.scss'
 
 export function ISCCRegistration() {
-  const [url, setUrl] = useState<string>(
-    'https://www.tagesschau.de/multimedia/bilder/spahn-425~_v-videowebl.jpg'
-  )
+  const [url, setUrl] = useState<string>('')
   const [isccCodes, setIsccCodes] = useState<ISCCCode[]>([])
   const [isWorking, setIsWorking] = useState(false)
+  const [isUrlInvalid, setIsUrlInvalid] = useState(false)
 
   const handleSumit = async (event: FormEvent) => {
     console.log(url)
     event.preventDefault()
     setIsWorking(true)
+
     const response = await fetch(`${API_PATH}/generate/from_url?url=${url}`, {
       method: 'POST',
-    }).finally(() => setIsWorking(false))
-    const json = await response.json()
-    setIsccCodes([...isccCodes, json])
+    })
+    if (response.status === 200) {
+      const json = await response.json()
+      setIsccCodes([...isccCodes, json])
+      setIsUrlInvalid(false)
+    } else {
+      setIsUrlInvalid(true)
+    }
+    setIsWorking(false)
+  }
+
+  const handleFile = async (files: File[]) => {
+    const newCodes = await Promise.all(
+      files.map(async (file) => {
+        const formData = new FormData()
+        formData.append('file', file)
+        const response = await fetch(`${API_PATH}/generate/from_file`, {
+          method: 'POST',
+          body: formData,
+        })
+        return (await response.json()) as ISCCCode
+      })
+    )
+
+    setIsccCodes([...isccCodes, ...newCodes])
   }
 
   return (
     <div className="container">
       <h1 className="title is1">ISCC Registration</h1>
       <form onSubmit={(event) => handleSumit(event)}>
+        <div className="field">
+          <label className="label">Generate from file</label>
+          <Dropzone onDrop={(acceptedFiles) => handleFile(acceptedFiles)}>
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()} className="dropzone">
+                  <input {...getInputProps()} />
+                  <p>
+                    <span className="icon">
+                      <i className="fas fa-download" />
+                    </span>
+                  </p>
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </div>
         <div className="field">
           <label className="label">Generate from URL</label>
           <div className="control">
@@ -35,15 +77,19 @@ export function ISCCRegistration() {
               onChange={(event) => setUrl(event.target.value)}
             />
           </div>
+          {isUrlInvalid ? (
+            <p className="help is-danger">This URL does not work</p>
+          ) : (
+            ''
+          )}
         </div>
-
         <div className="field">
           <div className="control">
             <input
               type="submit"
               className="button is-link"
               value="Submit"
-              disabled={isWorking}
+              disabled={isWorking || !url}
             />
             {isWorking ? (
               <span className="icon is-medium">
