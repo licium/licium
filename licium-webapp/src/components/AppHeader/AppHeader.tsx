a408f59b8b4698677a72c58e9d114208bd66c07a
Andreas Houben <andreas.houben@codecentric.de>
2020-09-13 14:40:42 +0200

    link in header

--- licium-webapp/src/components/AppHeader/AppHeader.tsx
+++ licium-webapp/src/components/AppHeader/AppHeader.tsx
@@ -3,10 +3,12 @@ import { Logo } from '../Logo/Logo'
 import './AppHeader.scss'
 
 export default function AppHeader() {
-  return (
-    <div className="app-header">
-      <Logo size={'sm'} />
-      licium
-    </div>
-  )
+    return (
+        <div className="app-header">
+            <a href={'/'}>
+                <Logo size={'sm'} />
+            </a>
+            licium
+        </div>
+    )
 }
