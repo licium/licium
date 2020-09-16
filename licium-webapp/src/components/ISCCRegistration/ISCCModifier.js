51e3b55874b53f01ac6ab2cad0fcd7982c65fc12
Andreas Houben <andreas.houben@codecentric.de>
2020-09-16 23:42:49 +0200

    Remove old ISCCRegistration.js

--- licium-webapp/src/components/ISCCRegistration/ISCCModifier.js
+++
@@ -1,9 +0,0 @@
-export const replaceMetaInfoOnISCC = (iscc, metaId) => ({
-    ...iscc,
-    iscc: iscc.iscc.replace(/\w*/, metaId.code),
-    bits: [metaId.bits, ...iscc.bits.slice(1)],
-    title: metaId.title,
-    title_trimmed: metaId.title_trimmed,
-    extra: metaId.extra,
-    extra_trimmed: metaId.extra_trimmed,
-})
--- licium-webapp/src/components/ISCCRegistration/ISCCServiceAdapter.js
+++
@@ -1,9 +0,0 @@
-import { API_PATH } from '../../App'
-
-export const generateMetaId = async (metaIdRequest) => {
-    const response = await fetch(`${API_PATH}/generate/meta_id`, {
-        method: 'POST',
-        body: JSON.stringify(metaIdRequest),
-    })
-    return await response.json()
-}
