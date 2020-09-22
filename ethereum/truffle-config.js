2e64a26628a40aa6011894e0fd109c82eefe229e
Kevin Wittek <kevin@wittek.dev>
2020-09-22 12:09:49 +0200

    Revert "exclude bloxberg network if no private key available"
    This reverts commit 7cefd9d1a1a1c7e52c005192d1d4c0cadfb4ec5d.

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const privateKey = process.env.PRIVATE_KEY
 
-const config = {
+module.exports = {
 
   contracts_build_directory: path.join(
     __dirname,
@@ -18,6 +18,16 @@ const config = {
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
+
+    bloxberg: {
+      provider: new HDWalletProvider(privateKey, 'https://core.bloxberg.org'),
+      //host: "https://core.bloxberg.org",
+     // port: 443, // Standard Ethereum port (default: none)
+      network_id: '*',
+      production: true,
+      //gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+      gasPrice: 200000000000,  // 200 gwei (default: 100 gwei)
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -39,23 +49,4 @@ const config = {
       // }
     },
   },
-
 };
-
-if (privateKey) {
-  config.networks = {
-    ...config.networks,
-    bloxberg: {
-      provider: new HDWalletProvider(privateKey, 'https://core.bloxberg.org'),
-      //host: "https://core.bloxberg.org",
-      // port: 443, // Standard Ethereum port (default: none)
-      network_id: '*',
-      production: true,
-      //gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      gasPrice: 200000000000,  // 200 gwei (default: 100 gwei)
-    }
-  }
-}
-
-
-module.exports = config;
