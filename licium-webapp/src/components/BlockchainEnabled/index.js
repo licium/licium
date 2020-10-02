b467687983d9ced42fdf901c7ed2428ecc5e2390
Andreas Houben <andreas.houben@codecentric.de>
2020-10-02 21:20:14 +0200

    Create pseudo login site to prepare Magic.link

--- licium-webapp/src/components/BlockchainEnabled/index.js
+++ licium-webapp/src/components/BlockchainEnabled/index.js
@@ -1,6 +1,60 @@
+import React, { useState } from 'react'
 import Web3 from 'web3'
+import {
+    Button,
+    Heading,
+    Input,
+    Link,
+    Stack,
+    Text,
+    Tooltip,
+} from '@chakra-ui/core'
+import Flex from '@chakra-ui/core/dist/Flex'
+import { Logo } from '../Logo/Logo'
+import { ReactComponent as MetamaskLogo } from './metamask-fox.svg'
 
 export const BlockchainEnabled = ({ children }) => {
-    window.web3 = new Web3(window.ethereum)
-    return children
+    const [providerChosen, setProviderChosen] = useState(
+        () => !!window.web3.eth.currentProvider
+    )
+
+    const activateMetamask = async () => {
+        window.web3 = new Web3(window.ethereum)
+        await window.web3.eth.requestAccounts()
+        setProviderChosen(true)
+    }
+    return providerChosen ? (
+        children
+    ) : (
+        <Flex
+            h="100vh"
+            justifyContent="center"
+            alignItems="center"
+            flexDir="column"
+        >
+            <Logo size="sm" />
+            <Heading as="h1" size="lg">
+                Welcome to licium
+            </Heading>
+            <Tooltip label="Available soon" aria-label="Available soon">
+                <form>
+                    <Stack spacing={2}>
+                        <Input disabled placeholder="Please enter your email" />
+                        <Button disabled type="submit">
+                            Login
+                        </Button>
+                    </Stack>
+                </form>
+            </Tooltip>
+            <div>-</div>
+            <Link onClick={() => activateMetamask()}>
+                <Flex flexDir="column" alignItems="center">
+                    <MetamaskLogo />
+                    <Text fontSize="sm">
+                        Click the icon if you prefer metamask
+                    </Text>
+                </Flex>
+            </Link>
+        </Flex>
+    )
 }
--- licium-webapp/src/components/Logo/Logo.js
+++ licium-webapp/src/components/Logo/Logo.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { ReactComponent as LogoSVG } from './Logo.svg'
 import './Logo.scss'
 
-export function Logo(props) {
-    return <LogoSVG className={props.size} />
+export function Logo({ size }) {
+    return <LogoSVG className={size} />
 }
