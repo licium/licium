099911c9d649f4d3fee0a6ff0669eef695c7d91f
Andreas Houben <andreas.houben@codecentric.de>
2020-10-02 14:50:47 +0200

    Only open Metamask when it's necessary (not at the startup anymore)
    This is already a preparation for using Magic

--- licium-webapp/src/components/BlockchainEnabled/index.js
+++ licium-webapp/src/components/BlockchainEnabled/index.js
@@ -1,53 +1,6 @@
-import React, { useState } from 'react'
-import { Link, Text } from '@chakra-ui/core'
 import Web3 from 'web3'
 
-export const BlockchainEnabled = (props) => {
-    const [isMetaMaskUnlocked, setMetaMaskUnlocked] = useState(false)
-
-    const eth = window.ethereum
-
-    const bootStrapMetamask = async () => {
-        eth.on('connect', () => setMetaMaskUnlocked(true))
-        eth.on('disconnect', () => setMetaMaskUnlocked(false))
-        eth.on('accountsChanged', (accounts) => {
-            if (accounts.length === 0) {
-                setMetaMaskUnlocked(false)
-            }
-        })
-        const accounts = await eth.request({
-            method: 'eth_requestAccounts',
-        })
-        if (accounts) {
-            setMetaMaskUnlocked(true)
-        }
-    }
-
-    const ethEnabled = () => {
-        if (eth) {
-            window.web3 = new Web3(window.ethereum)
-            bootStrapMetamask()
-            return true
-        }
-        return false
-    }
-    return (
-        <>
-            {ethEnabled() ? (
-                isMetaMaskUnlocked ? (
-                    props.children
-                ) : (
-                    <div>Please unlock Metamask</div>
-                )
-            ) : (
-                <Text>
-                    Please install{' '}
-                    <Link href="https://metamask.io/" target="_blank">
-                        Metamask
-                    </Link>{' '}
-                    to use this dApp!
-                </Text>
-            )}
-        </>
-    )
+export const BlockchainEnabled = ({ children }) => {
+    window.web3 = new Web3(window.ethereum)
+    return children
 }
