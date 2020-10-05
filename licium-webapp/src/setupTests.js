09fea759cf1bdcf591deaf34af65ef70d3707698
Andreas Houben <andreas.houben@codecentric.de>
2020-10-05 21:23:17 +0200

    remove enzyme config

--- licium-webapp/src/setupTests.js
+++ licium-webapp/src/setupTests.js
@@ -3,11 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import { server } from './mocks/server'
-
-configure({ adapter: new Adapter() })
 
 beforeAll(() => {
     // Enable the mocking in tests.
